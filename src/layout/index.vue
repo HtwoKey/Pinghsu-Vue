e5fe634e3be4bcb444ece7e16cbffeb19f6c9c67
HtwoKey <38719091+HtwoKey@users.noreply.github.com>
2022-05-27 14:24:17 +0800

    Add files via upload

---
+++ src/layout/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <Header />
+    <app-main />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import { AppMain, Header, Footer } from './components'
+export default {
+  name: 'Layout',
+  components: {
+    Header,
+    Footer,
+    AppMain
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+</style>
